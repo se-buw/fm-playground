643cc8933becd972d979f3504dcab4a8e92426e6
soaibsafi <soaib.safi@gmail.com>
2023-10-01 13:34:56 +0200

    change docker compose config

--- backend/config.py
+++ backend/config.py
@@ -2,11 +2,12 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-username = os.getenv('DB_USERNAME', 'postgres')
-password = os.getenv('DB_PASSWORD', 'postgres')
-host = os.getenv('DB_HOST', 'postgres')
-port = os.getenv('DB_PORT', '5432')
-database = os.getenv('DB_NAME', 'postgres')
+username = os.getenv('DB_USERNAME')
+password = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+port = os.getenv('DB_PORT')
+database = os.getenv('DB_NAME')
+app_secret = os.getenv('APP_SECKET_KEY')
 flask_env = os.getenv('FLASK_ENV')
 class Config:
   DEBUG = True
@@ -16,8 +17,7 @@ class Config:
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = f"postgresql://{username}:{password}@{host}:{port}/{database}"
   CACHE_TYPE = 'simple'
-  
   SESSION_TYPE = 'filesystem'
-  SECRET_KEY = 'your_secret_key'
+  SECRET_KEY = app_secret
   SESSION_PERMANENT = True
   PERMANENT_SESSION_LIFETIME = 3600  # Example: 1 hour
