fbc1771dce93e2697e8d07b3c220689e950821ba
soaibsafi <soaib.safi@gmail.com>
2023-10-07 17:26:28 +0200

    update backend for queries

--- backend/routes.py
+++ backend/routes.py
@@ -9,17 +9,7 @@ from utils.db_query import *
 import pytz
 aware_datetime = datetime.now(pytz.utc)
 
-
 routes = Blueprint('routes', __name__)
-
-
-CHECK = {
-  0: "VAL",
-  1: "SAT",
-  2: "QBF",
-  3: "SMT",
-  4: "SMV"  
-}
 
 TIME_WINDOW = 1 # time window within which multiple requests are not allowed
 
@@ -32,20 +22,12 @@ def is_valid_size(code: str) -> bool:
 
 @routes.route('/api/')
 def index():
-    # Get the current time
     current_time = datetime.now(pytz.utc)
-    # Check if the last request time is stored in the session
     last_request_time = session.get('last_request_time')
-    # print(f"Last request time: {last_request_time}")
-
     if last_request_time is not None and current_time - last_request_time < timedelta(seconds=TIME_WINDOW):
-        # If the last request was within the time window, deny the request
         return abort(429)  # HTTP status code for "Too Many Requests"
 
-    # Update the session with the current time
     session['last_request_time'] = current_time
-
-    # Process the request normally
     return "Request accepted"
   
 
@@ -54,7 +36,7 @@ def index():
 def save():
   current_time = datetime.now(pytz.utc)
   data = request.get_json()
-  check = CHECK.get(data['check'], None)
+  check_type = data['check']
   code = data['code'].strip()
   parent = data['parent']
   
@@ -63,7 +45,6 @@ def save():
     return response
   
   last_request_time = session.get('last_request_time')
-  # print(f"Last request time: {last_request_time}")
  
   # Allow one request per 5 seconds and same check
   if last_request_time is not None and current_time - last_request_time < timedelta(seconds=TIME_WINDOW):
@@ -71,24 +52,22 @@ def save():
     return response
   
   try:
-    exist_same_check = code_exists_in_db_for_same_check(check, code)
+    exist_same_check = code_exists_in_db_for_same_check(check_type, code)
     if exist_same_check is not None: # Exist: Do not save again
       permalink = exist_same_check.permalink
-      # print(f"Exist: {permalink}")
       session['last_request_time'] = current_time
-      response = make_response(jsonify({'permalink': permalink}), 200)
+      response = make_response(jsonify({'check':check_type,'permalink': permalink}), 200)
       return response
     
     exist_different_check = code_exists_in_db_different_check(code)
-    if exist_different_check is not None: # Exist but different check
-      permalink = f"{data['check']}{exist_different_check.permalink[1:]}" # replace the check type only
-      # print(f"Exist but different check: {permalink}")
-    else: # New: Generate a new permalink
-      permalink = generate_passphrase(data['check'])
-
     session_id = session.sid
-    # print(f"Session ID: {session_id}")
-    new_data = Data( time= datetime.now(), session_id=session_id, parent=parent, check_type=check, code=code, permalink=permalink)
+    if exist_different_check is not None: # Exist but different check
+      permalink = exist_different_check.permalink # replace the check type only
+      new_data = Data( time= datetime.now(), session_id=session_id, parent=parent, check_type=check_type, code=code, permalink=permalink)
+    else: # New: Generate a new permalink
+      permalink = generate_passphrase()
+      new_data = Data( time= datetime.now(), session_id=session_id, parent=parent, check_type=check_type, code=code, permalink=permalink)
+
     db.session.add(new_data)
     db.session.commit()
       
@@ -98,14 +77,15 @@ def save():
     return response
   
   session['last_request_time'] = current_time
-  # print(session)
-  response = make_response(jsonify({'permalink': permalink}), 200)
+  response = make_response(jsonify({'check':check_type,'permalink': permalink}), 200)
   return response
   
 
-@routes.route('/api/<permalink>', methods=['GET'])
-def get_code(permalink):
-  data = Data.query.filter_by(permalink=permalink).first_or_404()
+@routes.route('/api/permalink/', methods=['GET'])
+def get_code():
+  check = request.args.get('check')
+  p = request.args.get('p')
+  data = Data.query.filter_by(permalink=p).filter_by(check_type=check).first_or_404()
   response = make_response(jsonify({'code': data.code}))
   return response
 
@@ -120,12 +100,10 @@ def run_nuxmv():
     return {'error': "The code is too large."}, 413
 
   last_request_time = session.get('last_request_time')
-  #last_check_type = session.get('last_check_type')
-  
+
   if last_request_time is not None and current_time - last_request_time < timedelta(seconds=TIME_WINDOW):
     response = make_response(jsonify({'error': "You've already made a request recently."}), 429)
     return response
-  
   try:
     res = xmv.process_commands(code)
     response = make_response(jsonify({'result': res}), 200)
--- backend/utils/db.py
+++ backend/utils/db.py
@@ -1,13 +1,6 @@
-import os
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-# username = os.getenv('DB_USERNAME', 'postgres')
-# password = os.getenv('DB_PASSWORD', 'postgres')
-# host = os.getenv('DB_HOST', 'postgres')
-# port = os.getenv('DB_PORT', '5432')
-# database = os.getenv('DB_NAME', 'postgres')
 
 class Data(db.Model):
   __tablename__ = 'data'
@@ -16,8 +9,8 @@ class Data(db.Model):
   session_id = db.Column(db.String())
   parent = db.Column(db.String())
   check_type = db.Column(db.String(3))
-  code = db.Column(db.String())
   permalink = db.Column(db.String())
+  code = db.Column(db.String())
 
 def init_app(app):
   db.init_app(app)
