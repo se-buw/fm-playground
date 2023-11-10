978e04431cb6631442fe6a5eef9423012fc0ab46
Soaibuzzaman <soaib.safi@gmail.com>
2023-11-10 17:47:15 +0100

    :card_file_box:migrate db wtih metadata

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
@@ -33,3 +34,4 @@ def init_app(app):
   with app.app_context():
     print("Creating tables...")
     db.create_all()
+    migrate = Migrate(app, db)
--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 from flask_cors import CORS
 from flask_caching import Cache
-from flask import Flask,render_template, send_from_directory, request
-import requests
+from flask import Flask,render_template, send_from_directory
 app = Flask(__name__)
 CORS(app)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
@@ -12,26 +11,6 @@ app.config['APPLICATION_ROOT'] = "/"
 @app.route("/")
 def index():
   return render_template("index.html")
-
-# @app.route('/val')
-# def validity():
-#   return render_template("limboole/limboole.html", selected_option=0)
-
-# @app.route('/sat')
-# def Satisfiability():
-#   return render_template("limboole/limboole.html", selected_option=1)
-
-# @app.route('/qbf')
-# def  QBFSatisfiability():
-#   return render_template("limboole/limboole.html", selected_option=2)
-
-# @app.route('/smt')
-# def  smt():
-#   return render_template("smt/smt.html", selected_option=3)
-
-# @app.route('/xmv')
-# def  nuxmv():
-#   return render_template("xmv/xmv.html", selected_option=4)
 
 
 @app.route('/coi-serviceworker.js')
@@ -77,12 +56,6 @@ def limboole_wasm():
 @app.route('/favicon.ico')
 def fevicon():
      return send_from_directory('static', 'favicon.ico')
-
-@app.route('/proxy', methods=['GET'])
-def proxy():
-    url = request.args.get('url')
-    response = requests.get(url)
-    return response.content, response.status_code, response.headers.items()
 
 if __name__ == '__main__':
     app.run(port=5000)
