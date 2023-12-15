c5932abf7988f7f5d036cf3ee5f47d38d15ae88c
Soaibuzzaman <soaib.safi@gmail.com>
2023-12-15 21:11:33 +0100

    :sparkles:add login with github

--- frontend/src/components/Authentication/Login.jsx
+++ frontend/src/components/Authentication/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import {
   MDBBtn,
   MDBContainer,
@@ -15,29 +15,37 @@ function Login() {
 
 
   function handleGoogleLogin() {
-    window.open(`${import.meta.env.VITE_FMP_API_URL}/login`, '_self')
+    window.open(`${import.meta.env.VITE_FMP_API_URL}/login/google`, '_self')
+  }
+
+  function handleGithubLogin() {
+    window.open(`${import.meta.env.VITE_FMP_API_URL}/login/github`, '_self')
   }
   return (
-      <MDBContainer className='d-flex justify-content-center align-items-center' style={{ height: '100vh' }}>
+    <MDBContainer className='d-flex justify-content-center align-items-center' style={{ height: '100vh' }}>
 
-        <MDBCard className='shadow-5' style={{ width: '30rem' }}>
-          <h3 className='mx-auto mt-5'>Login with your identity provider</h3>
-          
-          <MDBCardBody className='p-5 shadow-5 text-center'>
-          
-            <MDBBtn style={{ backgroundColor: '#dd4b39' }}
-              onClick={handleGoogleLogin}
-            >
-              <MDBIcon className='me-2' fab icon='google' /> Login with Google
-            </MDBBtn>
-            <br />
-            <br />
-            {/* <MDBBtn style={{ backgroundColor: '#000000' }} href='#'>
-              <MDBIcon className='me-2' fab icon='github' /> Login with Github
-            </MDBBtn> */}
-          </MDBCardBody>
-        </MDBCard>
-      </MDBContainer>
+      <MDBCard className='shadow-5' style={{ width: '30rem' }}>
+        <h3 className='mx-auto mt-5'>Login with your identity provider</h3>
+
+        <MDBCardBody className='p-5 shadow-5 text-center'>
+
+          <MDBBtn
+            style={{ backgroundColor: '#dd4b39' }}
+            onClick={handleGoogleLogin}
+          >
+            <MDBIcon className='me-2' fab icon='google' /> Login with Google
+          </MDBBtn>
+          <br />
+          <br />
+          <MDBBtn
+            style={{ backgroundColor: '#000000' }}
+            onClick={handleGithubLogin}
+          >
+            <MDBIcon className='me-2' fab icon='github' /> Login with Github
+          </MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBContainer>
   );
 }
 
