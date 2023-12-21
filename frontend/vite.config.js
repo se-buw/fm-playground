02dd3fbd1f31bb52562d95001c0971b77d9f1125
Soaibuzzaman <soaib.safi@gmail.com>
2023-12-21 12:31:01 +0100

    :wrench:Update docker compose for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     },
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
-    port: 5173, // you can replace this port with any port
+    port: 5173, 
   }
 })
  
