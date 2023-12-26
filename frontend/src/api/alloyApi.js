cfd93ef82634a0feef734fd7992a22f86faa7b6c
Soaibuzzaman <soaib.safi@gmail.com>
2023-12-26 16:13:27 +0100

    :sparkles:Start alloy-app migration from meteor to react

---
+++ frontend/src/api/alloyApi.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { saveCode } from "./playgroundApi";
+
+const ALLOY_API_URL = import.meta.env.VITE_ALLOY_API_URL;
+const MAX_INSTANCES = import.meta.env.VITE_ALLOY_MAX_INSTANCES;
+
+/**
+ * Method to execute the current model and get model instances.
+ * This will call the Alloy API (webService). If the model contains
+ * secrets and the previous didn't (if any), will become a new derivation
+ * root (although it still registers the derivation).
+ *
+ * @param {String} code the Alloy model to execute
+ * @param {Number} commandIndex the index of the command to execute
+ * @param {String} currentModelId the id of the current model (from which the new will derive)
+ *
+ * @returns the instance data and the id of the new saved model
+ */
+export async function getInstances(code, commandIndex, currentModelId) {
+  let url = `${ALLOY_API_URL}/getInstances`;
+  // saveCode(code);
+  try {
+    const response = await axios.post(url, {
+      model: code,
+      numberOfInstances: MAX_INSTANCES,
+      commandIndex: commandIndex,
+      sessionId: currentModelId,
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
