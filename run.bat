2cfba4e0c6fad10e8a7974b2a0edb98c51be4259
Soaibuzzaman <soaib.safi@gmail.com>
2024-01-17 12:28:22 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,11 @@
 @echo on
 cd backend
+copy .env.example .env
 python -m venv .venv
 call .\.venv\Scripts\activate
 pip install -r requirements.txt
 start cmd /k "python app.py"
 
 cd ../frontend
+copy .env.example .env
 start cmd /k "npm install && npm run dev"
