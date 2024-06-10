e252867b1e5b806231975fad81dd6ef51d893c95
Soaibuzzaman <soaib.safi@gmail.com>
2024-06-10 18:49:27 +0200

    WIP-check cytoscape style

--- frontend/src/components/Playground/alloy/AlloyOutput.jsx
+++ frontend/src/components/Playground/alloy/AlloyOutput.jsx
@@ -1,15 +1,16 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import CytoscapeComponent from "react-cytoscapejs";
-
 
 const AlloyOutput = ({ code, onChange, height }) => {
   const cyRef = React.useRef(null);
-  // dummy elements
+
   const [elements, setElements] = useState([
     { data: { id: "a", label: "Node A" }, position: { x: 100, y: 100 } }, 
     { data: { id: "b", label: "Node B" }, position: { x: 200, y: 200 } },
-    { data: { source: "a", target: "b" } }
-  ])
+    { data: { source: "a", target: "b", label: "Edge A-B" } },
+    { data: { source: "b", target: "a", label: "Edge AB/A" } }
+  ]);
+
   const [layout, setLayout] = useState({ name: "grid" });
   const [stylesheet, setStylesheet] = useState([
     {
@@ -26,11 +27,16 @@ const AlloyOutput = ({ code, onChange, height }) => {
         width: 3,
         "line-color": "#ccc",
         "target-arrow-color": "#ccc",
-        "target-arrow-shape": "triangle"
+        "target-arrow-shape": "triangle", // Indicates direction with an arrow
+        "curve-style": "bezier", // Ensures the edge is a straight line with an arrow
+        label: "data(label)", // Display edge label
+        "text-rotation": "autorotate",
+        "font-size": 10,
+        "text-margin-y": -10
       }
     }
-  
-  ])
+  ]);
+
   return (
     <CytoscapeComponent
       elements={elements}
