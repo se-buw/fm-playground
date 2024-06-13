26e2956c99b814956109eaecde7562431ac81d09
Soaibuzzaman <soaib.safi@gmail.com>
2024-06-13 11:49:27 +0200

    WIP-alloy-api saving instance in tmp file

--- alloy-api/src/main/java/de/buw/fmp/alloy/api/AlloyInstanceController.java
+++ alloy-api/src/main/java/de/buw/fmp/alloy/api/AlloyInstanceController.java
@@ -12,18 +12,18 @@ import edu.mit.csail.sdg.translator.TranslateAlloyToKodkod;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.io.File;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.json.JSONObject;
 import org.json.XML;
+import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
 
 @RestController
 public class AlloyInstanceController {
@@ -33,8 +33,17 @@ public class AlloyInstanceController {
     @PostMapping("/instance/{cmd}")
     public String postMethodName(@RequestBody InstanceRequest instanceRequest, @PathVariable int cmd) throws IOException {
         String code = instanceRequest.getCode();
+        CompModule module = null;
+        try {
+            module = CompUtil.parseEverything_fromString(A4Reporter.NOP, code);
+        } catch (Exception e) {
+            // return error message as JSON with http status code 400
+            JSONObject obj = new JSONObject();
+            obj.put("error", e.toString());
+            obj.put("status", HttpStatus.BAD_REQUEST.value());
+            return obj.toString();
+        }
         // parse Alloy file from code variable
-        CompModule module = CompUtil.parseEverything_fromString(A4Reporter.NOP, code);
 
         A4Options options = new A4Options();
         // get the first instance of the Alloy file
@@ -49,9 +58,12 @@ public class AlloyInstanceController {
             // store the instance in the instances map
             instances.put(specId, new StoredSolution(instance));
         }
-        instance.writeXML("instance.xml");
+
+        File tmpFile = File.createTempFile("alloy_instance", ".xml");
+        tmpFile.deleteOnExit();
+        instance.writeXML(tmpFile.getAbsolutePath());
         // read content of instance.xml as String
-        String instanceContent = Files.readString(Paths.get("instance.xml"));
+        String instanceContent = Files.readString(Paths.get(tmpFile.getAbsolutePath()));
         JSONObject xmlJSONObj = XML.toJSONObject(instanceContent);
         if (specId != null) {
             xmlJSONObj.append("specId", specId);
@@ -71,9 +83,11 @@ public class AlloyInstanceController {
             storedSolution.setSolution(instance);
         }
 
-        instance.writeXML("instance.xml");
+        File tmpFile = File.createTempFile("alloy_instance", ".xml");
+        tmpFile.deleteOnExit();
+        instance.writeXML(tmpFile.getAbsolutePath());
         // read content of instance.xml as String
-        String instanceContent = Files.readString(Paths.get("instance.xml"));
+        String instanceContent = Files.readString(Paths.get(tmpFile.getAbsolutePath()));
         JSONObject xmlJSONObj = XML.toJSONObject(instanceContent);
 
         xmlJSONObj.append("specId", specId);
