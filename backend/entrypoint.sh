3a1f639759e541a7d0d5bc26b95375bbfd4e5726
Soaibuzzaman <soaib.safi@gmail.com>
2024-07-26 12:35:26 +0200

    Changed flask thread

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn --bind 0.0.0.0:8000 app:app --timeout 120 --workers 4 --threads 2 --worker-class gthread
+gunicorn --bind 0.0.0.0:8000 app:app --timeout 120 --workers 8 --threads 1 --worker-class gthread
