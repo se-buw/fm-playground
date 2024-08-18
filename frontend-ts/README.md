99d3017cf25da0c27babe6818f689134d40b502d
Soaibuzzaman <soaib.safi@gmail.com>
2024-08-18 09:41:12 +0200

    :tada:Init frontend with typescript

---
+++ frontend-ts/README.md
@@ -0,0 +1,1 @@
+# FM Playground - Frontend TypeScript
---
+++ frontend-ts/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>FM Playground</h1>
+    </>
+  )
+}
+
+export default App
