8d5af2f84ba6638a2b2e6e319f37b7164f9160ef
soaibsafi <soaib.safi@gmail.com>
2024-10-09 11:57:11 +0200

    🗃️nuxmv-api configured default redis with/without

--- nuxmv-api/main.py
+++ nuxmv-api/main.py
@@ -3,42 +3,63 @@ import requests
 from typing import Union
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
+
 load_dotenv()
 from nuxmv import process_commands
-from fmp_redis.cache_decorator import fmp_redis_cache
+import redis
+from redis_cache import RedisCache
 
 API_URL = os.getenv("API_URL")
 REDIS_URL = os.getenv("REDIS_URL")
 
+client = redis.Redis.from_url(REDIS_URL)
+cache = RedisCache(redis_client=client)
+
 app = FastAPI()
 
-@fmp_redis_cache(REDIS_URL)
+
+def is_redis_available() -> bool:
+    try:
+        r = redis.Redis.from_url(REDIS_URL)
+        r.ping()
+        return True
+    except redis.ConnectionError:
+        return False
+
+
 def run_nuxmv(code: str) -> str:
-  try:
-    return process_commands(code)
-  except:
-    raise HTTPException(status_code=500, detail="Error running code")
+    if is_redis_available():
+
+        @cache.cache()
+        def cached_run_nuxmv(code: str) -> str:
+            return process_commands(code)
+
+        try:
+            return cached_run_nuxmv(code)
+        except:
+            raise HTTPException(status_code=500, detail="Error running nuXmv cli")
+    else:
+        try:
+            return process_commands(code)
+        except:
+            raise HTTPException(status_code=500, detail="Error running nuXmv cli")
 
 
 def get_code_by_permalink(check: str, p: str) -> Union[str, None]:
-  try:
-    if check.upper() == "XMV":
-      url = f"{API_URL}api/permalink/?check={check}&p={p}"
-      res = requests.get(url)
-      code = res.json().get("code")
-      return code
-  except:
-    raise HTTPException(status_code=404, detail="Permalink not found")
-
+    try:
+        if check.upper() == "XMV":
+            url = f"{API_URL}api/permalink/?check={check}&p={p}"
+            res = requests.get(url)
+            code = res.json().get("code")
+            return code
+    except:
+        raise HTTPException(status_code=404, detail="Permalink not found")
 
 
 @app.get("/xmv/run/", response_model=None)
 def code(check: str, p: str):
-  try:
     code = get_code_by_permalink(check, p)
-  except:
-    raise HTTPException(status_code=404, detail="Permalink not found")
-  try:
-    return run_nuxmv(code)
-  except:
-    raise HTTPException(status_code=500, detail="Error running code")
+    try:
+        return run_nuxmv(code)
+    except:
+        raise HTTPException(status_code=500, detail="Error running code")
