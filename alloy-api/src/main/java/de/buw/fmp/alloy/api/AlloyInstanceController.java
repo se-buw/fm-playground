06e3f65a32a14d9e0b80ef2fc5a72ddd922c2c10
soaibsafi <soaib.safi@gmail.com>
2024-10-11 21:26:20 +0200

    ♻️refactored alloy api with permalink

--- alloy-api/src/main/java/de/buw/fmp/alloy/api/AlloyInstanceController.java
+++ alloy-api/src/main/java/de/buw/fmp/alloy/api/AlloyInstanceController.java
@@ -1,6 +1,7 @@
 package de.buw.fmp.alloy.api;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import edu.mit.csail.sdg.alloy4.A4Reporter;
 import edu.mit.csail.sdg.alloy4.Pos;
@@ -25,15 +26,18 @@ import java.util.concurrent.TimeoutException;
 
 import org.json.JSONObject;
 import org.json.XML;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class AlloyInstanceController {
-
+    @Value("${API_URL:https://play.formal-methods.net/}")
+    private String apiUrl;
     public static int TIME_OUT = 60;
     public static int MAX_RUNNING = 10;
 
@@ -44,14 +48,33 @@ public class AlloyInstanceController {
     public static A4Options getOptions() {
         A4Options opt = new A4Options();
         // String osName = System.getProperty("os.name").toLowerCase();
-        // opt.solver = osName.contains("linux") ? A4Options.SatSolver.MiniSatJNI : A4Options.SatSolver.SAT4J;
+        // opt.solver = osName.contains("linux") ? A4Options.SatSolver.MiniSatJNI :
+        // A4Options.SatSolver.SAT4J;
         opt.solver = A4Options.SatSolver.SAT4J;
         return opt;
-      }
+    }
 
-    @PostMapping("/instance/{cmd}")
-    public String getInstance(@RequestBody InstanceRequest instanceRequest, @PathVariable int cmd) throws IOException {
-        String code = instanceRequest.getCode();
+    @GetMapping("/alloy/instance")
+    public String getInstance(
+            @RequestParam(required = true) String check,
+            @RequestParam(required = true) String p,
+            @RequestParam(required = true) int cmd) throws IOException {
+
+        if (!"ALS".equalsIgnoreCase(check)) {
+            JSONObject obj = new JSONObject();
+            obj.put("error", "Invalid Permalink");
+            obj.put("status", HttpStatus.BAD_REQUEST.value());
+            return obj.toString();
+        }
+
+        String code = getCodeByPermalink(check, p);
+        if (code == null) {
+            JSONObject obj = new JSONObject();
+            obj.put("error", "Invalid Permalink");
+            obj.put("status", HttpStatus.BAD_REQUEST.value());
+            return obj.toString();
+        }
+
         CompModule module = null;
         // parse Alloy file from code variable
         try {
@@ -64,10 +87,12 @@ public class AlloyInstanceController {
             return obj.toString();
         }
 
-        Command runCommand = module.getAllCommands().get(cmd);        
+        Command runCommand = module.getAllCommands().get(cmd);
         if (cmd == 0 && hasDefaultCommand(module)) {
-            runCommand = new Command(Pos.UNKNOWN, ExprConstant.TRUE, "FMPlayDefault", false, 4, 4, 4, -1, -1, -1, null, null, ExprConstant.TRUE, null);
-        };
+            runCommand = new Command(Pos.UNKNOWN, ExprConstant.TRUE, "FMPlayDefault", false, 4, 4, 4, -1, -1, -1, null,
+                    null, ExprConstant.TRUE, null);
+        }
+        ;
 
         A4Options options = getOptions();
         // get the first instance of the Alloy file
@@ -97,7 +122,7 @@ public class AlloyInstanceController {
             obj.put("status", HttpStatus.BAD_REQUEST.value());
             return obj.toString();
         }
-        
+
         // generate a unieuq id for the instance
         do {
             specId = Long.toHexString(Double.doubleToLongBits(Math.random()));
@@ -119,8 +144,21 @@ public class AlloyInstanceController {
         String textInstance = instance.toString();
         xmlJSONObj.append("textInstance", textInstance);
         String jsonPrettyPrintString = xmlJSONObj.toString(4);
-        
+
         return jsonPrettyPrintString;
+    }
+
+    public String getCodeByPermalink(String check, String p) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String url = apiUrl + "api/permalink/?check=" + check + "&p=" + p;
+            String response = restTemplate.getForObject(url, String.class);
+            JSONObject obj = new JSONObject(response);
+            return obj.getString("code");
+
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     private boolean hasDefaultCommand(CompModule module) {
@@ -128,12 +166,12 @@ public class AlloyInstanceController {
             Command cmd = module.getAllCommands().get(0);
             if (Pos.UNKNOWN.equals(cmd.pos)) {
                 return true;
-            }            
+            }
         }
         return false;
     }
 
-    @PostMapping("/nextInstance")
+    @PostMapping("/alloy/nextInstance")
     public String getNextInstance(@RequestBody String specId) throws IOException {
         StoredSolution storedSolution = instances.get(specId);
         if (storedSolution == null) {
@@ -166,7 +204,7 @@ public class AlloyInstanceController {
             obj.put("status", HttpStatus.BAD_REQUEST.value());
             return obj.toString();
         }
-        
+
         storedSolution.setSolution(instance);
         File tmpFile = File.createTempFile("alloy_instance", ".xml");
         tmpFile.deleteOnExit();
@@ -198,13 +236,13 @@ public class AlloyInstanceController {
         }
         Thread t = new Thread(r);
         t.start();
-        
+
         // wait for the thread to finish or timeout
         running++;
         t.join(seconds * 1000);
         running--;
 
-        if (t.isAlive()) {            
+        if (t.isAlive()) {
             t.stop();
             throw new TimeoutException("Analysis timed out after " + seconds + " seconds.");
         }
@@ -222,12 +260,11 @@ public class AlloyInstanceController {
 
         @Override
         public void run() {
-            try {                
+            try {
                 instance = runInstance();
             } catch (Throwable t) {
                 reasonForFailing = t;
             }
         }
     }
-    
 }
