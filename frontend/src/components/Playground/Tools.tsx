8ec6f62b99abfad49a95ac6bf00748ee4a8c6e66
soaibsafi <soaib.safi@gmail.com>
2024-10-14 18:34:19 +0200

    ♻️Restructuring needed when fmp.config is ready

--- frontend/src/components/Playground/Tools.tsx
+++ frontend/src/components/Playground/Tools.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../../assets/style/Playground.css'
 import Select from 'react-select';
 import fmpConfig from '../../../fmp.config';
-
+import Options from '../../assets/config/AvailableTools'
 
 export type LanguageProps = {
   id: string;
@@ -12,10 +12,11 @@ export type LanguageProps = {
 }
 
 interface ToolsProps {
-  onChange: (selectedOption: any) => void; // TODO: maybe change this to LanguageProps
+  onChange: (selectedOption: any) => void; // TODO: Change any to LanguageProps when the fmp.config.ts file is ready
   selected: LanguageProps;
 }
 
+// TODO: Use it when the fmp.config.ts file is ready
 const options = Object.values(fmpConfig.tools).map(tool => ({
   id: tool.name.toLowerCase(),
   value: tool.dropdown.value,
@@ -25,21 +26,22 @@ const options = Object.values(fmpConfig.tools).map(tool => ({
 
 
 const Tools: React.FC<ToolsProps> = (props: ToolsProps) => {
-  const [optionsState, setOptionsState] = useState(options);
+  // FIXME: This is a temporary solution. The options should be generated from the fmp.config.ts file.
+  const [options, setOptions] = useState(Options);
 
   return (
-      <div className='tools'>     {/* TODO: fix this  */}
+      <div className='tools'> 
         <Select
           className="basic-single react-select-container"
           classNamePrefix="select"
-          defaultValue={optionsState[0]}
+          defaultValue={options[0]}
           isDisabled={false}
           isLoading={false}
           isClearable={false}
           isRtl={false}
           isSearchable={true}
           name="color"
-          options={optionsState}
+          options={options}
           onChange={props.onChange}
           value={props.selected}
         />
