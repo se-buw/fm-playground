a398cffe130054ba704868c705529124a9e271e1
Soaibuzzaman <soaib.safi@gmail.com>
2024-12-27 11:20:44 +0100

    🔥already checked inside run_limboole

--- frontend/src/assets/ts/toolExecutor/limbooleExecutor.ts
+++ frontend/src/assets/ts/toolExecutor/limbooleExecutor.ts
@@ -36,19 +36,7 @@ export const executeLimboole = async (
     showErrorModal(`Something went wrong. If the problem persists, open an <a href="${fmpConfig.issues}" target="_blank">issue</a>`);
     setIsExecuting(false);
   }
-
-
   const infoElement = document.getElementById('info');
-  const nonAscii = findNonAscii(editorValue);
-  if (nonAscii !== -1) {
-    setLineToHighlight([nonAscii.line]);
-    setIsExecuting(false);
-    if (infoElement) {
-      infoElement.innerHTML = `<i style='color: red;'>The code contains non-ASCII characters. Please remove the character '${nonAscii.char}' at line ${nonAscii.line}, column ${nonAscii.column} and try again.</i>`;
-    }
-    return;
-  }
-
   run_limboole(window.Wrappers[limbooleCheckOption.value], editorValue);
 
   if (infoElement) {
@@ -56,14 +44,3 @@ export const executeLimboole = async (
   }
   setIsExecuting(false);
 };
-
-const findNonAscii = (str: string) => {
-  const regex = /[^\x00-\x7F]/g;
-  const match = regex.exec(str);
-  if (!match) return -1;
-  // find the line and column and the non-ascii character
-  const line = (str.substring(0, match.index).match(/\n/g) || []).length + 1;
-  const column = match.index - str.lastIndexOf('\n', match.index);
-  const char = match[0];
-  return { line, column, char };
-}
