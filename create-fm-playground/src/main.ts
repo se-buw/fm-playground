b866e9f201a7ae5ed73875ef38c00306e3ebf572
Soaibuzzaman <soaib.safi@gmail.com>
2025-05-30 19:52:52 +0200

    Removed CLI to create fm-playground from main

--- create-fm-playground/src/main.ts
+++
@@ -1,9 +0,0 @@
-export {
-    createFMPlayground,
-    generateToolMaps,
-    updateViteConfig,
-    copySelectiveGuidesJson,
-    copySelectiveLineHighlightingUtil,
-} from './index.js';
-export * from './types.js';
-export * from './tool-map-template.js';
